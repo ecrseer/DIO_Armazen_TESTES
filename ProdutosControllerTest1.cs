d9cdc44126c8b5b06f0cff5499c8332c84ded936
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-28 20:53:20 -0300

    updating: net 5

---
+++ ProdutosControllerTest1.cs
@@ -0,0 +1,44 @@
+
+using Armazen_API.Controllers;
+using DIO_Armazen.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DIO_Armazen_TESTES
+{
+    public class ProdutosControllerTest1
+    {
+        private readonly Mock<DbSet<Produto>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Produto _produto;
+        public ProdutosControllerTest1() {
+            _mockSet = new Mock<DbSet<Produto>>();
+            _mockContext = new Mock<Context>();
+            _produto = new Produto { id=32,descricao="Desodorante ivone"};
+            _mockContext.Setup(mck => mck.produtos).Returns(
+                _mockSet.Object);//retorna o objt
+            _mockContext.Setup(mck => mck.produtos.FindAsync(32))
+                .ReturnsAsync(_produto);//retorna metodo utilizado
+        }
+        
+        [Fact]
+        public async Task getCategoriax_()
+        {
+            var servico = new ProdutosController(_mockContext.Object);
+            await servico.GetProduto(32);
+
+            _mockSet.Verify(mymck => mymck.FindAsync(32), Times.Once());
+
+        }
+
+
+
+
+
+
+        
+    }
+}
---
+++ UtilitariosControllerTest1.cs
@@ -0,0 +1,41 @@
+
+using Armazen_API.Controllers;
+using DIO_Armazen.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DIO_Armazen_TESTES
+{
+    public class UtilitariosControllerTest1
+    {
+        private readonly Mock<DbSet<Utilitario>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Utilitario _utilitario;
+        public UtilitariosControllerTest1() {
+            _mockSet = new Mock<DbSet<Utilitario>>();
+            _mockContext = new Mock<Context>();
+            _utilitario = new Utilitario { id=32,descricao="Eletrodomesticos"};
+            _mockContext.Setup(m => m.Utilitarios).Returns(_mockSet.Object);
+        }
+        
+        [Fact]
+        public async Task get_Utilitario()
+        {
+            var servico = new UtilitariosController(_mockContext.Object);
+            await servico.GetUtilitario(32);
+
+            _mockSet.Verify(mymck => mymck.FindAsync(32), Times.Once());
+
+        }
+
+
+
+
+
+
+        
+    }
+}
